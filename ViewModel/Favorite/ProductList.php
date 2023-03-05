eb587c30e78442e455e4a1f15fdbc922eaec0bed
Denis <guimaraes.dev@gmail.com>
2023-03-05 12:15:48 +0100

    ajout pagination à la collection de produits favoris

--- ViewModel/Favorite/ProductList.php
+++ ViewModel/Favorite/ProductList.php
@@ -6,22 +6,28 @@ use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\Catalog\Model\ResourceModel\Product\Collection;
 use Magento\Catalog\Model\Product\Visibility;
+use Magento\Customer\Model\Session;
 use Magento\Framework\App\RequestInterface;
+use SimpleMage\SimpleFavorites\Model\ResourceModel\FavoriteLink;
 
 class ProductList implements ArgumentInterface
 {
+    private const DEFAULT_PAGE_SIZE = 12;
     private CollectionFactory $collectionFactory;
     private Visibility $productVisibility;
     private Collection $productCollection;
+    private Session $customerSession;
     private RequestInterface $request;
 
     public function __construct(
         CollectionFactory $collectionFactory,
         Visibility $productVisibility,
+        Session $customerSession,
         RequestInterface $request
     ) {
         $this->collectionFactory = $collectionFactory;
         $this->productVisibility = $productVisibility;
+        $this->customerSession = $customerSession;
         $this->request = $request;
     }
 
@@ -35,18 +41,34 @@ class ProductList implements ArgumentInterface
 
     private function loadProductCollection(): void
     {
-        $collection = $this->collectionFactory->create();
-        $collection->addAttributeToSelect('*');
-        $collection->setVisibility($this->productVisibility->getVisibleInSiteIds());
-        $collection->joinField(
+        $this->productCollection = $this->collectionFactory->create();
+        $this->productCollection->addAttributeToSelect('*');
+        $this->productCollection->setVisibility($this->productVisibility->getVisibleInSiteIds());
+        $this->productCollection->joinField(...$this->getCreatedAtJoinParameters());
+        $this->productCollection->addOrder('favorite_created_at', 'DESC');
+        $this->productCollection->setPageSize($this->getPageSize());
+        $this->productCollection->setCurPage($this->getCurrentPage());
+        $this->productCollection->load();
+    }
+
+    private function getCreatedAtJoinParameters(): array
+    {
+        return [
             'favorite_created_at',
-            $collection->getTable('simple_favorites'),
+            FavoriteLink::TABLE_NAME_FAVORITE,
             'created_at',
             'product_id = entity_id',
-            ['customer_id' => 2]
-        );
-        $collection->addOrder('favorite_created_at', 'ASC');
-        $collection->load();
-        $this->productCollection = $collection;
+            ['customer_id' => $this->customerSession->getCustomerId()]
+        ];
+    }
+
+    private function getPageSize(): int
+    {
+        return $this->request->getParam('product_list_limit') ?: self::DEFAULT_PAGE_SIZE;
+    }
+
+    private function getCurrentPage(): int
+    {
+        return $this->request->getParam('p') ?: 1;
     }
 }
